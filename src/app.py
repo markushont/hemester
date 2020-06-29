aa6afc7a509d2abec0b59b309a5a372af2bdc888
Markus Silberstein Hont <markus@silberstein.nu>
2020-06-29 21:12:57 +0200

    initial commit

---
+++ src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, escape, request
+import requests
+
+app = Flask(__name__)
+
+G_BASE_URL = 'https://maps.googleapis.com/maps/api'
+G_API_KEY = 'AIzaSyDCyqNwpSz753CdHoRP4DCcMHEoM7k0aPE'
+
+@app.route('/api/route', methods=['GET'])
+def get_route():
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
+
+    route_response = requests.get(
+        f'{G_BASE_URL}/directions/json?origin={origin}&destination={destination}&key={G_API_KEY}'
+    )
+
+    if not route_response.ok:
+        return { 'error_code': route_response.status_code, 'message': route_response.text }
+    
+    location = route_response.json().get('routes')[0].get('legs')[0].get('end_location')
+    print(route_response.json().get('routes')[0].get('legs')[0])
+    location_str = ','.join([str(location.get('lat')), str(location.get('lng'))])
+    print(location_str)
+
+    places_response = requests.get(
+        f'{G_BASE_URL}/place/nearbysearch/json?location={location_str}&radius=5000&key={G_API_KEY}&type=roofing_contractor'
+    )
+
+    if not places_response.ok:
+        return { 'error_code': route_response.status_code, 'message': route_response.text }
+    
+    return places_response.json()
+
